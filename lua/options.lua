802a6ff5ea6af8db47eca6a986d50311bfbcebc0
Roman <NecroRomnt@yandex.ru>
2024-07-12 16:57:27 +0400

    Добавлены символы

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,10 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+
+local signs = { Error = "󰅙 ", Warn = " ", Hint = " ", Info = "🛈 ", Debug = " "}
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
